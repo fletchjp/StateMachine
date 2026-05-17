ee8c06241edd02279460dec1d14d1b4e66a2e669
John Fletcher <john@bunbury28.plus.com>
2026-05-17 12:46:39 +0100

    Update StateMachine_R4.ino
    Revise comment

--- ArduinoR4/StateMachine_R4/StateMachine_R4.ino
+++ ArduinoR4/StateMachine_R4/StateMachine_R4.ino
@@ -5,8 +5,8 @@
 // I want now to have an equivalent for the UNO R3 or MEGA 
 // using the embedded template library.
 
-// The problem with this style of code is that for N event types
-// there are N^2 lambdas to be written.
+// The problem with this style of code is that for N event types and M states
+// there are N*M lambdas to be written.
 
 // This is a C++17 file from the work of Tamir Bahar
 // https://tamir.dev/posts/a-functional-style-state-macnine-in-cpp
